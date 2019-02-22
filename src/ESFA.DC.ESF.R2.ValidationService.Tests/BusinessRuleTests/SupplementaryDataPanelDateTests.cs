f21efc68d48643b1d65eca6a2428f95253619a40
jamie <jamie.coales@education.gov.uk>
2019-02-22 13:07:18 +0000

    SupplementaryDataPanelDate01

---
+++ src/ESFA.DC.ESF.R2.ValidationService.Tests/BusinessRuleTests/SupplementaryDataPanelDateTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.ESF.R2.Models;
+using ESFA.DC.ESF.R2.ValidationService.Commands.BusinessRules;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ESF.R2.ValidationService.Tests.BusinessRuleTests
+{
+    public class SupplementaryDataPanelDateTests
+    {
+        [Fact]
+        public void SupplementaryDataPanelDate01FailsDateInFuture()
+        {
+            var suppData = new SupplementaryDataModel
+            {
+                SupplementaryDataPanelDate = new DateTime(2018, 9, 2)
+            };
+
+            var mock = new Mock<IDateTimeProvider>();
+            mock
+                .Setup(m => m.GetNowUtc())
+                .Returns(new DateTime(2018, 9, 1));
+
+            var rule = new SupplementaryDataPanelDate01(mock.Object);
+
+            Assert.False(rule.IsValid(suppData));
+        }
+
+        [Fact]
+        public void SupplementaryDataPanelDate01PassesNoSupplementaryDataPanelDate()
+        {
+            var suppData = new SupplementaryDataModel
+            {
+                SupplementaryDataPanelDate = null
+            };
+
+            var mock = new Mock<IDateTimeProvider>();
+
+            var rule = new SupplementaryDataPanelDate01(mock.Object);
+
+            Assert.True(rule.IsValid(suppData));
+        }
+
+        [Fact]
+        public void SupplementaryDataPanelDate0PassesSupplementaryDataPanelDateNotInFuture()
+        {
+            var suppData = new SupplementaryDataModel
+            {
+                SupplementaryDataPanelDate = new DateTime(2018, 9, 1)
+            };
+
+            var mock = new Mock<IDateTimeProvider>();
+            mock
+                .Setup(m => m.GetNowUtc())
+                .Returns(new DateTime(2018, 9, 1));
+
+            var rule = new SupplementaryDataPanelDate01(mock.Object);
+
+            Assert.True(rule.IsValid(suppData));
+        }
+    }
+}
