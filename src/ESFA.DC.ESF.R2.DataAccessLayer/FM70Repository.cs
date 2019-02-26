82fbc35e6e5918a9c98d95ec01db71e2dcca5a97
jamie <jamie.coales@education.gov.uk>
2019-02-26 14:05:10 +0000

    remove try catch from repos

--- src/ESFA.DC.ESF.R2.DataAccessLayer/FM70Repository.cs
+++ src/ESFA.DC.ESF.R2.DataAccessLayer/FM70Repository.cs
@@ -9,49 +9,39 @@ using ESFA.DC.ESF.R2.Models;
 using ESFA.DC.ESF.R2.Utils;
 using ESFA.DC.ILR1819.DataStore.EF;
 using ESFA.DC.ILR1819.DataStore.EF.Interfaces;
-using ESFA.DC.Logging.Interfaces;
 
 namespace ESFA.DC.ESF.R2.DataAccessLayer
 {
     public class FM70Repository : IFM70Repository
     {
         private readonly Func<IILR1819_DataStoreEntities> _validContext;
-        private readonly ILogger _logger;
 
         public FM70Repository(
-            Func<IILR1819_DataStoreEntities> context,
-            ILogger logger)
+            Func<IILR1819_DataStoreEntities> context)
         {
             _validContext = context;
-            _logger = logger;
         }
 
         public async Task<ILRFileDetailsModel> GetFileDetails(int ukPrn, CancellationToken cancellationToken)
         {
-            ILRFileDetailsModel fileDetail = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            ILRFileDetailsModel fileDetail;
 
-                using (var context = _validContext())
-                {
-                    fileDetail = await context.FileDetails
-                        .Where(fd => fd.UKPRN == ukPrn)
-                        .OrderBy(fd => fd.SubmittedTime)
-                        .Select(fd => new ILRFileDetailsModel
-                        {
-                            FileName = fd.Filename,
-                            LastSubmission = fd.SubmittedTime
-                        })
-                        .FirstOrDefaultAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get file details with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                fileDetail = await context.FileDetails
+                    .Where(fd => fd.UKPRN == ukPrn)
+                    .OrderBy(fd => fd.SubmittedTime)
+                    .Select(fd => new ILRFileDetailsModel
+                    {
+                        FileName = fd.Filename,
+                        LastSubmission = fd.SubmittedTime
+                    })
+                    .FirstOrDefaultAsync(cancellationToken);
             }
 
             if (fileDetail != null && !string.IsNullOrEmpty(fileDetail.FileName))
@@ -64,24 +54,18 @@ namespace ESFA.DC.ESF.R2.DataAccessLayer
 
         public async Task<IList<ESF_LearningDelivery>> GetLearningDeliveries(int ukPrn, CancellationToken cancellationToken)
         {
-            IList<ESF_LearningDelivery> values = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            IList<ESF_LearningDelivery> values;
 
-                using (var context = _validContext())
-                {
-                    values = await context.ESF_LearningDelivery
-                        .Where(v => v.UKPRN == ukPrn)
-                        .ToListAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get FM70 learningDeliveries with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                values = await context.ESF_LearningDelivery
+                    .Where(v => v.UKPRN == ukPrn)
+                    .ToListAsync(cancellationToken);
             }
 
             return values;
@@ -89,24 +73,18 @@ namespace ESFA.DC.ESF.R2.DataAccessLayer
 
         public async Task<IList<ESF_LearningDeliveryDeliverable>> GetLearningDeliveryDeliverables(int ukPrn, CancellationToken cancellationToken)
         {
-            IList<ESF_LearningDeliveryDeliverable> values = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            IList<ESF_LearningDeliveryDeliverable> values;
 
-                using (var context = _validContext())
-                {
-                    values = await context.ESF_LearningDeliveryDeliverable
-                        .Where(v => v.UKPRN == ukPrn)
-                        .ToListAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get FM70 LearningDeliveryDeliverables with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                values = await context.ESF_LearningDeliveryDeliverable
+                    .Where(v => v.UKPRN == ukPrn)
+                    .ToListAsync(cancellationToken);
             }
 
             return values;
@@ -114,24 +92,18 @@ namespace ESFA.DC.ESF.R2.DataAccessLayer
 
         public async Task<IList<ESF_LearningDeliveryDeliverable_Period>> GetLearningDeliveryDeliverablePeriods(int ukPrn, CancellationToken cancellationToken)
         {
-            IList<ESF_LearningDeliveryDeliverable_Period> values = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            IList<ESF_LearningDeliveryDeliverable_Period> values;
 
-                using (var context = _validContext())
-                {
-                    values = await context.ESF_LearningDeliveryDeliverable_Period
-                        .Where(v => v.UKPRN == ukPrn)
-                        .ToListAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get FM70 LearningDeliveryDeliverablePeriods with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                values = await context.ESF_LearningDeliveryDeliverable_Period
+                    .Where(v => v.UKPRN == ukPrn)
+                    .ToListAsync(cancellationToken);
             }
 
             return values;
@@ -139,45 +111,39 @@ namespace ESFA.DC.ESF.R2.DataAccessLayer
 
         public async Task<IList<FM70PeriodisedValuesModel>> GetPeriodisedValues(int ukPrn, CancellationToken cancellationToken)
         {
-            IList<FM70PeriodisedValuesModel> values = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            IList<FM70PeriodisedValuesModel> values;
 
-                using (var context = _validContext())
-                {
-                    values = await context.ESF_LearningDeliveryDeliverable_PeriodisedValues
-                        .Where(v => v.UKPRN == ukPrn)
-                        .Select(v => new FM70PeriodisedValuesModel
-                        {
-                            FundingYear = 2018,
-                            UKPRN = v.UKPRN,
-                            LearnRefNumber = v.LearnRefNumber,
-                            DeliverableCode = v.DeliverableCode,
-                            AimSeqNumber = v.AimSeqNumber,
-                            AttributeName = v.AttributeName,
-                            Period1 = v.Period_1,
-                            Period2 = v.Period_2,
-                            Period3 = v.Period_3,
-                            Period4 = v.Period_4,
-                            Period5 = v.Period_5,
-                            Period6 = v.Period_6,
-                            Period7 = v.Period_7,
-                            Period8 = v.Period_8,
-                            Period9 = v.Period_9,
-                            Period10 = v.Period_10,
-                            Period11 = v.Period_11,
-                            Period12 = v.Period_12
-                        })
-                        .ToListAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get FM70 periodised values with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                values = await context.ESF_LearningDeliveryDeliverable_PeriodisedValues
+                    .Where(v => v.UKPRN == ukPrn)
+                    .Select(v => new FM70PeriodisedValuesModel
+                    {
+                        FundingYear = 2018,
+                        UKPRN = v.UKPRN,
+                        LearnRefNumber = v.LearnRefNumber,
+                        DeliverableCode = v.DeliverableCode,
+                        AimSeqNumber = v.AimSeqNumber,
+                        AttributeName = v.AttributeName,
+                        Period1 = v.Period_1,
+                        Period2 = v.Period_2,
+                        Period3 = v.Period_3,
+                        Period4 = v.Period_4,
+                        Period5 = v.Period_5,
+                        Period6 = v.Period_6,
+                        Period7 = v.Period_7,
+                        Period8 = v.Period_8,
+                        Period9 = v.Period_9,
+                        Period10 = v.Period_10,
+                        Period11 = v.Period_11,
+                        Period12 = v.Period_12
+                    })
+                    .ToListAsync(cancellationToken);
             }
 
             return values;
@@ -185,24 +151,18 @@ namespace ESFA.DC.ESF.R2.DataAccessLayer
 
         public async Task<IList<ESF_DPOutcome>> GetOutcomes(int ukPrn, CancellationToken cancellationToken)
         {
-            IList<ESF_DPOutcome> values = null;
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+            IList<ESF_DPOutcome> values;
 
-                using (var context = _validContext())
-                {
-                    values = await context.ESF_DPOutcome
-                        .Where(v => v.UKPRN == ukPrn)
-                        .ToListAsync(cancellationToken);
-                }
-            }
-            catch (Exception ex)
+            if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogError($"Failed to get FM70 DPOutcomes with ukPrn {ukPrn}", ex);
+                return null;
+            }
+
+            using (var context = _validContext())
+            {
+                values = await context.ESF_DPOutcome
+                    .Where(v => v.UKPRN == ukPrn)
+                    .ToListAsync(cancellationToken);
             }
 
             return values;
--- src/ESFA.DC.ESF.R2.DataAccessLayer/ValidRepository.cs
+++ src/ESFA.DC.ESF.R2.DataAccessLayer/ValidRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading;
@@ -7,139 +6,99 @@ using System.Threading.Tasks;
 using ESFA.DC.ESF.R2.Interfaces.DataAccessLayer;
 using ESFA.DC.ILR1819.DataStore.EF.Valid;
 using ESFA.DC.ILR1819.DataStore.EF.Valid.Interfaces;
-using ESFA.DC.Logging.Interfaces;
 
 namespace ESFA.DC.ESF.R2.DataAccessLayer
 {
     public class ValidRepository : IValidRepository
     {
         private readonly IILR1819_DataStoreEntitiesValid _context;
-        private readonly ILogger _logger;
 
         public ValidRepository(
-            IILR1819_DataStoreEntitiesValid context,
-            ILogger logger)
+            IILR1819_DataStoreEntitiesValid context)
         {
             _context = context;
-            _logger = logger;
         }
 
         public async Task<List<Learner>> GetLearners(int ukPrn, CancellationToken cancellationToken)
         {
-            List<Learner> learners = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                learners = await _context.Learners.Where(l => l.UKPRN == ukPrn).ToListAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid learners with ukPrn {ukPrn}", ex);
-            }
+            var learners = await _context.Learners
+                .Where(l => l.UKPRN == ukPrn)
+                .ToListAsync(cancellationToken);
 
             return learners;
         }
 
         public async Task<List<LearningDelivery>> GetLearningDeliveries(int ukPrn, CancellationToken cancellationToken)
         {
-            List<LearningDelivery> deliveries = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                deliveries = await _context.LearningDeliveries.Where(l => l.UKPRN == ukPrn && l.FundModel == 70).ToListAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid LearningDeliveries with ukPrn {ukPrn}", ex);
-            }
+            var deliveries = await _context.LearningDeliveries
+                .Where(l => l.UKPRN == ukPrn && l.FundModel == 70)
+                .ToListAsync(cancellationToken);
 
             return deliveries;
         }
 
         public async Task<List<LearningDeliveryFAM>> GetLearningDeliveryFAMs(int ukPrn, CancellationToken cancellationToken)
         {
-            List<LearningDeliveryFAM> deliveryFams = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                deliveryFams = await _context.LearningDeliveryFAMs.Where(l => l.UKPRN == ukPrn).ToListAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid LearningDeliveryFAMs with ukPrn {ukPrn}", ex);
-            }
+            var deliveryFams = await _context.LearningDeliveryFAMs
+                .Where(l => l.UKPRN == ukPrn)
+                .ToListAsync(cancellationToken);
 
             return deliveryFams;
         }
 
         public async Task<List<ProviderSpecLearnerMonitoring>> GetProviderSpecLearnerMonitorings(int ukPrn, CancellationToken cancellationToken)
         {
-            List<ProviderSpecLearnerMonitoring> monitorings = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                monitorings = await _context.ProviderSpecLearnerMonitorings.Where(l => l.UKPRN == ukPrn).ToListAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid ProviderSpecLearnerMonitorings with ukPrn {ukPrn}", ex);
-            }
+            var monitorings = await _context.ProviderSpecLearnerMonitorings
+                .Where(l => l.UKPRN == ukPrn)
+                .ToListAsync(cancellationToken);
 
             return monitorings;
         }
 
         public async Task<List<ProviderSpecDeliveryMonitoring>> GetProviderSpecDeliveryMonitorings(int ukPrn, CancellationToken cancellationToken)
         {
-            List<ProviderSpecDeliveryMonitoring> monitorings = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                monitorings = await _context.ProviderSpecDeliveryMonitorings.Where(l => l.UKPRN == ukPrn).ToListAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid ProviderSpecDeliveryMonitorings with ukPrn {ukPrn}", ex);
-            }
+            var monitorings = await _context.ProviderSpecDeliveryMonitorings
+                .Where(l => l.UKPRN == ukPrn)
+                .ToListAsync(cancellationToken);
 
             return monitorings;
         }
 
         public async Task<List<DPOutcome>> GetDPOutcomes(int ukPrn, CancellationToken cancellationToken)
         {
-            List<DPOutcome> outcomes = null;
-            try
+            if (cancellationToken.IsCancellationRequested)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return null;
-                }
+                return null;
+            }
 
-                outcomes = await _context.DPOutcomes.Where(l => l.UKPRN == ukPrn).ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to get valid DPOutcomes with ukPrn {ukPrn}", ex);
-            }
+            var outcomes = await _context.DPOutcomes
+                .Where(l => l.UKPRN == ukPrn)
+                .ToListAsync(cancellationToken);
 
             return outcomes;
         }
