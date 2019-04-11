beffed1a6a9bc92fbb3d8897db3a789d153e5bef
jamie <jamie.coales@education.gov.uk>
2019-04-11 10:03:14 +0100

    ConRefNumber02

---
+++ src/ESFA.DC.ESF.R2.ValidationService.Tests/BusinessRuleTests/ConRefNumberRuleTests.cs
@@ -0,0 +1,37 @@
+﻿using ESFA.DC.ESF.R2.Models;
+using ESFA.DC.ESF.R2.ValidationService.Commands.BusinessRules;
+using Xunit;
+
+namespace ESFA.DC.ESF.R2.ValidationService.Tests.BusinessRuleTests
+{
+    public class ConRefNumberRuleTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("ESF-5000")]
+        public void ConRefNumber02PassesForEmptyOrValidConRefNumber(string conRefNumber)
+        {
+            var model = new SupplementaryDataModel
+            {
+                ConRefNumber = conRefNumber
+            };
+
+            var rule = new ConRefNumberRule02();
+
+            Assert.True(rule.IsValid(model));
+        }
+
+        [Fact]
+        public void ConRefNumber02FailsWithInvalidConRefNumber()
+        {
+            var model = new SupplementaryDataModel
+            {
+                ConRefNumber = "ESF-4999"
+            };
+
+            var rule = new ConRefNumberRule02();
+
+            Assert.False(rule.IsValid(model));
+        }
+    }
+}
---
+++ src/ESFA.DC.ESF.R2.ValidationService/Commands/BusinessRules/ConRefNumberRule02.cs
@@ -0,0 +1,31 @@
+﻿using ESFA.DC.ESF.R2.Models;
+using ESFA.DC.ESF.R2.Utils;
+
+namespace ESFA.DC.ESF.R2.ValidationService.Commands.BusinessRules
+{
+    public class ConRefNumberRule02
+    {
+        public string ErrorMessage => "The ConRefNumber is not a valid ConRefNumber for ESF Round 2.";
+
+        public string ErrorName => "ConRefNumber_02";
+
+        public bool IsWarning => false;
+
+        public bool IsValid(SupplementaryDataModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.ConRefNumber))
+            {
+                return true;
+            }
+
+            var numericString = model.ConRefNumber.Replace(ESFConstants.ConRefNumberPrefix, string.Empty);
+
+            if (!int.TryParse(numericString, out var contractNumber))
+            {
+                return true;
+            }
+
+            return contractNumber >= ESFConstants.ESFRound2StartConRefNumber;
+        }
+    }
+}
