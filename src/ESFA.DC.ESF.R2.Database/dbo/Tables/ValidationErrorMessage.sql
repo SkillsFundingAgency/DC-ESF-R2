ff9018454c7f5054995e32684d5f2769d2941556
jamie <jamie.coales@education.gov.uk>
2019-05-15 12:49:01 +0100

    validation error messages database changes

---
+++ src/ESFA.DC.ESF.R2.Database/dbo/Tables/ValidationErrorMessage.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ValidationErrorMessage]
+(
+	[ValidationErrorMessageId]		INT				IDENTITY (1, 1) NOT NULL,
+	[RuleName]						VARCHAR (100),
+	[ErrorMessage]					VARCHAR (MAX),
+    CONSTRAINT [PK_ValidationErrorMessage] PRIMARY KEY CLUSTERED ([ValidationErrorMessageId] ASC)
+)
