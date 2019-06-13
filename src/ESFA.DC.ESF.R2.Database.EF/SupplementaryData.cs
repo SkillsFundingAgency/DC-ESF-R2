70f7ec6c3c7f9c1cb9ad707960324bd65394eb9f
jamie <jamie.coales@education.gov.uk>
2019-06-13 09:47:45 +0100

    table fixes

--- src/ESFA.DC.ESF.R2.Database.EF/SupplementaryData.cs
+++ src/ESFA.DC.ESF.R2.Database.EF/SupplementaryData.cs
@@ -10,7 +10,6 @@ namespace ESFA.DC.ESF.R2.Database.EF
         public int CalendarYear { get; set; }
         public int CalendarMonth { get; set; }
         public string CostType { get; set; }
-        public string StaffName { get; set; }
         public string ReferenceType { get; set; }
         public string Reference { get; set; }
         public long? Uln { get; set; }
