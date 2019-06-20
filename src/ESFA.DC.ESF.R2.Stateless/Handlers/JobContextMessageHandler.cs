a8922c0689b220ed99fbeb6361971bd988596d09
jamie <jamie.coales@education.gov.uk>
2019-06-20 11:25:16 +0100

    fix for cache per call

--- src/ESFA.DC.ESF.R2.Stateless/Handlers/JobContextMessageHandler.cs
+++ src/ESFA.DC.ESF.R2.Stateless/Handlers/JobContextMessageHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Autofac;
 using ESFA.DC.ESF.R2.Interfaces.Controllers;
 using ESFA.DC.ESF.R2.Stateless.Mappers;
 using ESFA.DC.JobContextManager.Interface;
@@ -11,34 +12,38 @@ namespace ESFA.DC.ESF.R2.Stateless.Handlers
 {
     public class JobContextMessageHandler : IMessageHandler<JobContextMessage>
     {
+        private readonly ILifetimeScope _lifetimeScope;
         private readonly ILogger _logger;
 
-        private readonly IServiceController _controller;
-
         public JobContextMessageHandler(
-            IServiceController controller,
+            ILifetimeScope lifetimeScope,
             ILogger logger)
         {
-            _controller = controller;
             _logger = logger;
+            _lifetimeScope = lifetimeScope;
         }
 
         public async Task<bool> HandleAsync(JobContextMessage jobContextMessage, CancellationToken cancellationToken)
         {
-            _logger.LogInfo("ESF callback invoked");
-
-            var jobContextModel = JobContextMapper.MapJobContextToModel(jobContextMessage);
-            if (!jobContextModel.Tasks.Any())
+            using (var childLifetimeScope = _lifetimeScope.BeginLifetimeScope())
             {
-                _logger.LogInfo("ESF. No tasks to run.");
+                _logger.LogInfo("ESF callback invoked");
+
+                var jobContextModel = JobContextMapper.MapJobContextToModel(jobContextMessage);
+                if (!jobContextModel.Tasks.Any())
+                {
+                    _logger.LogInfo("ESF. No tasks to run.");
+                    return true;
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var controller = childLifetimeScope.Resolve<IServiceController>();
+
+                await controller.RunTasks(jobContextModel, cancellationToken);
+
                 return true;
             }
-
-            cancellationToken.ThrowIfCancellationRequested();
-
-            await _controller.RunTasks(jobContextModel, cancellationToken);
-
-            return true;
         }
     }
 }
