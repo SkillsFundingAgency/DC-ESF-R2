606d1a0f63e872b0aec012dcad8fa02ab98a29d0
jamie <jamie.coales@education.gov.uk>
2019-10-21 10:12:22 +0100

    Reference_03 validation rule

---
+++ src/ESFA.DC.ESF.R2.ValidationService/Commands/BusinessRules/ReferenceRule03.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using ESFA.DC.ESF.R2.Interfaces.DataAccessLayer;
+using ESFA.DC.ESF.R2.Interfaces.Validation;
+using ESFA.DC.ESF.R2.Models;
+
+namespace ESFA.DC.ESF.R2.ValidationService.Commands.BusinessRules
+{
+    public class ReferenceRule03 : BaseValidationRule, IBusinessRuleValidator
+    {
+        private const string Pattern = @"^[A-Z,a-z,0-9,\s]*$";
+
+        public ReferenceRule03(IValidationErrorMessageService errorMessageService)
+            : base(errorMessageService)
+        {
+        }
+
+        public override string ErrorName => "Reference_03";
+
+        public bool IsWarning => false;
+
+        public bool IsValid(SupplementaryDataModel model)
+        {
+            return !model.ReferenceType.Equals(Constants.ReferenceType_LearnRefNumber)
+                   || (string.IsNullOrEmpty(model.Reference) || Regex.IsMatch(model.Reference, Pattern));
+        }
+    }
+}
