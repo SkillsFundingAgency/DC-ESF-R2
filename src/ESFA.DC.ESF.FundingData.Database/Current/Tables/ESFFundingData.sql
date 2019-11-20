4e254bbd6bb36f6717b5e1b845de2f69b400f761
Anil Pabbati <anil.pabbati@outlook.com>
2019-11-20 16:23:53 +0000

    dbo.ESFFundingData view change to point to Current.EsfFundingData

--- src/ESFA.DC.ESF.FundingData.Database/Current/Tables/ESFFundingData.sql
+++ src/ESFA.DC.ESF.FundingData.Database/Current/Tables/ESFFundingData.sql
@@ -38,4 +38,14 @@
 GO
 
 CREATE INDEX [IX_ESFFundingData_UKPRN_CollectionType_CollectionReturnCode] ON [Current].[ESFFundingData] ([UKPRN] ASC, [CollectionType] ASC, [CollectionReturnCode] ASC)
-
+GO
+CREATE NONCLUSTERED INDEX [IX_ESFFundingData_All] ON [Current].[ESFFundingData]
+(
+	[UKPRN] ASC,
+	[ConRefNumber] ASC,
+	[DeliverableCode] ASC,
+	[AttributeName] ASC,
+	[CollectionType] ASC,
+	[CollectionReturnCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
