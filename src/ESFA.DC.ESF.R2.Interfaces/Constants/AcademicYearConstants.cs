8286ed7c7ea272e315908204ded7db2db085cf8f
Mike Childs <mike@mjcdev.co.uk>
2020-07-20 14:43:20 +0100

    Add plumbing for year specific registrations

---
+++ src/ESFA.DC.ESF.R2.Interfaces/Constants/AcademicYearConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.ESF.R2.Interfaces.Constants
+{
+    public static class AcademicYearConstants
+    {
+        public const int Year1920 = 1920;
+
+        public const int Year2021 = 2021;
+    }
+}
--- src/ESFA.DC.ESF.R2.Stateless/Handlers/JobContextMessageHandler.cs
+++ src/ESFA.DC.ESF.R2.Stateless/Handlers/JobContextMessageHandler.cs
@@ -2,8 +2,12 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
+using Autofac.Core;
+using ESFA.DC.ESF.R2.Interfaces;
+using ESFA.DC.ESF.R2.Interfaces.Constants;
 using ESFA.DC.ESF.R2.Interfaces.Controllers;
 using ESFA.DC.ESF.R2.Interfaces.Services;
+using ESFA.DC.ESF.R2.Service;
 using ESFA.DC.ESF.R2.Stateless.Mappers;
 using ESFA.DC.JobContext.Interface;
 using ESFA.DC.JobContextManager.Interface;
@@ -27,11 +31,12 @@ namespace ESFA.DC.ESF.R2.Stateless.Handlers
 
         public async Task<bool> HandleAsync(JobContextMessage jobContextMessage, CancellationToken cancellationToken)
         {
-            using (var childLifetimeScope = _lifetimeScope.BeginLifetimeScope())
+            var jobContextModel = JobContextMapper.MapJobContextToModel(jobContextMessage);
+
+            using (var childLifetimeScope = _lifetimeScope.BeginLifetimeScope(s => RegisterYearSpecificServices(s, jobContextModel)))
             {
                 _logger.LogInfo("ESF R2 callback invoked");
 
-                var jobContextModel = JobContextMapper.MapJobContextToModel(jobContextMessage);
                 if (!jobContextModel.Tasks.Any())
                 {
                     _logger.LogInfo("ESF R2. No tasks to run.");
@@ -45,6 +50,16 @@ namespace ESFA.DC.ESF.R2.Stateless.Handlers
                 await controller.RunTasks(jobContextModel, cancellationToken);
 
                 return true;
+            }
+        }
+
+        private void RegisterYearSpecificServices(ContainerBuilder container, IEsfJobContext esfJobContext)
+        {
+            if (esfJobContext.CollectionYear == AcademicYearConstants.Year1920)
+            {
+            }
+            else if (esfJobContext.CollectionYear == AcademicYearConstants.Year2021)
+            {
             }
         }
     }
